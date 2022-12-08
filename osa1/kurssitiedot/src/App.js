a71a78256baad711e87ca20cb70aaaf436ba3cf5
Tero Oravasaari <tero.oravasaari@upsert.fi>
2022-12-08 16:07:49 +0200

    Osa 1 tehtävä 2 / Part 1 excercise 2

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 
+
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
   )
 }
 
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} excercise={props.exercises1} />
+      <Part part={props.part2} excercise={props.exercises2} />
+      <Part part={props.part3} excercise={props.exercises3} />
     </div>
   )
 }
+
 
 const Total = (props) => {
   return (
@@ -46,7 +50,11 @@ const App = () => {
         part2={part2} exercises2={exercises2}
         part3={part3} exercises3={exercises3}
       />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   )
 }
