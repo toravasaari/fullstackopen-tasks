241e3678efc6ebad5aa5341282ac8262a3f6a4ba
Tero Oravasaari <tero.oravasaari@upsert.fi>
2022-12-09 09:17:00 +0200

    Osa 1 tehtävä 5 / Part 1 excercise 5

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 
@@ -16,7 +16,7 @@ const Part = (props) => {
 
 
 const Content = (props) => {
-  const [part1, part2, part3] = props.parts
+  const [part1, part2, part3] = props.course.parts
   return (
     <div>
       <Part part={part1} />
@@ -28,7 +28,7 @@ const Content = (props) => {
 
 
 const Total = (props) => {
-  const [part1, part2, part3] = props.parts
+  const [part1, part2, part3] = props.course.parts
   return (
     <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
   )
@@ -36,27 +36,29 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
