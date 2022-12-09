322d855d8da757d19d97c8535c8db7f7c54f38bd
Tero Oravasaari <tero.oravasaari@upsert.fi>
2022-12-09 14:47:50 +0200

    Osa 1 tehtävä 8 / Part 1 excercise 8 - unicafe step 3
    Eriytetty "Statistics" omaksi komponentikseen

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,5 +1,22 @@
 import React, { useState } from 'react'
 
+
+const Statistics = (props) => {
+  const {good, neutral, bad} = props
+  const total = good + neutral + bad
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average { (good - bad) / total }</p>
+      <p>positive { good / total * 100} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -11,13 +28,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average { (good - bad) / (good + neutral + bad) }</p>
-      <p>positive { good / (good + neutral + bad) * 100} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
